d6adf0e38510a5643c0233b89f78d11efe815e7b
MariaEdu-dp <144979698+MariaEdu-dp@users.noreply.github.com>
2024-09-09 17:06:02 -0300

    Update ncep_tmax.py

--- tmax/ncep_tmax.py
+++ tmax/ncep_tmax.py
@@ -2,9 +2,17 @@ import xarray as xr
 import pandas as pd
 import glob
 import os
+from pathlib import Path
+from colorama import Fore
+
+# Define o caminho do diretório que você deseja criar
+diretorio = Path("files")
+
+# Cria o diretório, incluindo diretórios pai, se necessário
+diretorio.mkdir(parents=True, exist_ok=True)
 
 # Mude o diretório de trabalho para a pasta desejada
-os.chdir('files')
+os.chdir("files")
 
 # Verifique se a mudança foi bem-sucedida
 print("Diretório de trabalho atual:", os.getcwd())
@@ -12,7 +20,7 @@ print("Diretório de trabalho atual:", os.getcwd())
 # Agora você pode executar operações de arquivo na pasta desejada
 # Por exemplo, listar arquivos na pasta
 for arquivo in os.listdir():
-    print(arquivo)
+    print({Fore.LIGHTGREEN} arquivo {Fore.RESET})
 
 date_ranges = [
     ("1982-06-01", "1983-08-01"),
@@ -33,7 +41,6 @@ for (start_date, end_date) in date_ranges:
     # Carregar o arquivo netCDF
     nc_ndvi = xr.open_dataset('http://apdrc.soest.hawaii.edu:80/dods/public_data/Reanalysis_Data/NCEP/NCEP2/monthly/gaussian_grid/tmax2m')
     var1 = nc_ndvi['tmax']
-    print(var1)
     data = var1.sel(lat=slice(-60, 15), lon=slice(270, 330), time=slice(start_date, end_date)) 
-    print(data)
+    print(f"{Fore.LIGHTGREEN} data {Fore.RESET})
     data.to_netcdf(f"{start_date}.nc", mode='w')
